eb08ab1d0ec6d3dbb51e1beca60e4d75ee4cc881
jixiuf <jixiuf@gmail.com>
2012-05-02 00:51:02 +0800

    upper

--- linux/upperwrapper.c
+++ linux/upperwrapper.c
@@ -15,7 +15,8 @@ int main(int argc, char *argv[]){
     perror("open filename ");
     exit(1);
   }
-  dup2(fd,STDIN_FILENO); /* 经此后，STDIN_FILENO指向fd所指的文件 */
+  dup2(fd,STDIN_FILENO); /* 经此后，STDIN_FILENO指向fd所指的文件 ,所有从stdin读的数据 实从fd文件 来而*/
+
   /* 而upper.c中是从 stdin中读数据， */
   /* 用exec后，原来打开的文件描述符仍然是打开的[37]。利用这一点可以实现I/O重定向。
      先看一个简单的例子，把标准输入转成大写然后打印到标准输出 */
