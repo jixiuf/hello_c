6f0a70b3adc947ec5d646ddcbb6f34965b70d42c
jixiuf <jixiuf@gmail.com>
2012-05-29 22:39:55 +0800

    un sock

---
+++ linux/unix_domain_socket/create_un.c
@@ -0,0 +1,13 @@
+//file name : create_un.c
+//created at: 2012年05月21日 星期一 00时43分34秒
+//author:  纪秀峰
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+
+  return 0;
+}
