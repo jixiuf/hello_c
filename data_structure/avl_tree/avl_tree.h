0b5287c82d98c43f67b0572ab1538713bd1eb7a5
jixiuf <jixiuf@gmail.com>
2012-06-10 18:22:18 +0800

    avl

---
+++ data_structure/avl_tree/avl_tree.h
@@ -0,0 +1,27 @@
+#ifndef AVL_TREE_H
+#define AVL_TREE_H
+
+typedef int Item;
+
+struct avl_node {
+  Item item;
+  struct avl_node * left;
+  struct avl_node * right;
+  int height;
+};
+
+typedef struct avl_node avl_node_t;
+
+struct avl_tree{
+  avl_node_t * root;
+  int size;
+  int (*item_cmp)(Item* item1,Item* item2);
+};
+
+typedef struct avl_tree avl_tree_t;
+
+void avl_init(avl_tree_t* tree,int (*item_cmp)(Item* item1,Item* item2));
+int avl_add(avl_tree_t* avl,Item item);
+int avl_size(avl_tree_t * avl);
+
+#endif
