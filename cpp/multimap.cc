8ff95acb4df55aaaf1e009d0f6433135d62931b4
jixiuf <jixiuf@gmail.com>
2012-09-01 00:35:50 +0800

    mmap

--- cpp/multimap.cc
+++ cpp/multimap.cc
@@ -27,9 +27,24 @@ void test2(){
   }
   cout<<endl;
 }
+void test3(){
+  multimap<string,int> mmap;
+  mmap.insert(make_pair("k",100));
+  mmap.insert(make_pair("k",200));
+  mmap.insert(make_pair("k2",200000));
+  pair<multimap<string,int>::iterator,
+       multimap<string,int>::iterator> range = mmap.equal_range("k");
+  while (range.first!= range.second){
+    cout<<"key="<<range.first->first<<",value="<<range.first->second<<endl;
+    range.first++;
+  }
+  cout<<endl;
+}
+
 
 int main(int argc, char *argv[]){
   test1();
   test2();
+  test3();
   return 0;
 }
