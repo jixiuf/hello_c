d3051e03f5f09851a4af79e61cfe0c010a639627
jixiuf <jixiuf@gmail.com>
2014-01-14 23:18:45 +0800

    imwrite

---
+++ opencv/Demo03imwrite.cpp
@@ -0,0 +1,27 @@
+//  -*- coding:utf-8 -*-
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{
+  // 从文件读一个img ,
+  Mat originMat  = cv::imread("/Users/jishuuhou/Pictures/m2w500hq85lt_original_XfKB_1b7d000041351191.jpg",CV_LOAD_IMAGE_COLOR);
+  if(! originMat.data )                              // Check for invalid input
+    {
+      cout <<  "Could not open or find the image" << std::endl ;
+      return -1;
+    }
+
+
+  cv::namedWindow("winname",WINDOW_AUTOSIZE);
+  // 在这个窗口中 显示上面加载的img
+  cv::imshow("winname", originMat);
+
+  // write to file
+  cv::imwrite("/tmp/out.jpg", originMat);
+  return 0;
+}
