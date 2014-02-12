680f79c1d5c01e05ff53674b80c5bfa0fd024cb8
jixiuf <jixiuf@gmail.com>
2014-02-12 15:22:42 +0800

    compareHist

---
+++ opencv/Demo36compareHist.cpp
@@ -0,0 +1,77 @@
+//  -*- coding:utf-8-dos -*-
+#include <iostream>
+#include <string>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <cstdio>
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[]){
+
+  Mat src_base,hsv_base;
+  Mat src_test1 ,hsv_test1;
+  Mat src_test2,hsv_test2;
+
+  Mat hsv_half_down;
+
+  src_base = imread("in_hand_1.jpg");
+  src_test1 = imread("in_hand_2.jpg");
+  src_test2= imread("in_hand_3.jpg");
+
+  // convert bgr 2 hsv
+  cvtColor(src_base, hsv_base, CV_BGR2HSV);
+  cvtColor(src_test1, hsv_test1, CV_BGR2HSV);
+  cvtColor(src_test2, hsv_test2, CV_BGR2HSV);
+
+  // 取图片的下半部
+  hsv_half_down=hsv_base(Range(hsv_base.rows/2, hsv_base.rows-1),Range(0,hsv_base.cols-1));
+
+  int h_bins=50,s_bins=60;
+  int histSize[]={h_bins,s_bins};
+
+  // // hue varies from 0 to 256, saturation from 0 to 180
+  float h_rangs[]={0,256};
+  float s_rangs[]={0,180};
+  const float* ranges[]={h_rangs,s_rangs};
+  // Use the o-th and 1-st channels
+  int channels[]={0,1};
+
+  /// Histograms
+  MatND hist_base;
+  MatND hist_half_down;
+  MatND hist_test1;
+  MatND hist_test2;
+
+  calcHist(&hsv_base, 1, channels, Mat(), hist_base, 2, histSize, ranges, true, false);
+  normalize(hist_base, hist_base, 0, 1, NORM_MINMAX, -1, Mat());
+
+  calcHist(&hsv_half_down, 1, channels, Mat(), hist_half_down, 2, histSize, ranges, true, false);
+  normalize(hist_half_down, hist_half_down, 0, 1, NORM_MINMAX, -1, Mat());
+
+  calcHist(&hsv_test1, 1, channels, Mat(), hist_test1, 2, histSize, ranges, true, false);
+  normalize(hist_test1, hist_test1, 0, 1, NORM_MINMAX, -1, Mat());
+
+  calcHist(&hsv_test2, 1, channels, Mat(), hist_test2, 2, histSize, ranges, true, false);
+  normalize(hist_test2, hist_test2, 0, 1, NORM_MINMAX, -1, Mat());
+  // 4种比较方法CV_COMP_CORREL CV_COMP_CHISQR  CV_COMP_INTERSECT CV_COMP_BHATTACHARYYA
+  /// Apply the histogram comparison methods
+  for( int i = 0; i < 4; i++ ) {
+    int compare_method = i;
+    double base_base = compareHist( hist_base, hist_base, compare_method ); // 自身进行比较
+    double base_half = compareHist( hist_base, hist_half_down, compare_method ); // 自身， 与自身的一半进行比较
+    double base_test1 = compareHist( hist_base, hist_test1, compare_method );
+    double base_test2 = compareHist( hist_base, hist_test2, compare_method );
+
+    printf( " Method [%d] Perfect, Base-Half, Base-Test(1), Base-Test(2) : %f, %f, %f, %f \n", i, base_base, base_half , base_test1, base_test2 );
+  }
+
+
+  // namedWindow("dst",WINDOW_NORMAL);
+  // imshow("dst",dst);
+
+  // waitKey(0);
+  return 0;
+}
